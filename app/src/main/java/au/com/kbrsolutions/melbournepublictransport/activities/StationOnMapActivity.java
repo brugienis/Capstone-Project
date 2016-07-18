43436e3ef8cc5e0fdeb62f11c3a2214c78c2b8a3
Bohdan Rugienis <bohdan@rugienis.com>
2016-07-19 06:59:28 +1000

    Create StationOnMapActivity.

---
+++ app/src/main/java/au/com/kbrsolutions/melbournepublictransport/activities/StationOnMapActivity.java
@@ -0,0 +1,15 @@
+package au.com.kbrsolutions.melbournepublictransport.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import au.com.kbrsolutions.melbournepublictransport.R;
+
+public class StationOnMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stationsonmap);
+    }
+}
