72f52ca3947516961871348b0573d5b582a0f0b8
Bohdan Rugienis <bohdan@rugienis.com>
2016-09-24 12:49:44 +1000

    Create NearbyStopsDetails and fragment_nearby_stops_list.

---
+++ app/src/main/java/au/com/kbrsolutions/melbournepublictransport/data/NearbyStopsDetails.java
@@ -0,0 +1,72 @@
+package au.com.kbrsolutions.melbournepublictransport.data;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class NearbyStopsDetails implements Parcelable {
+
+    public final String stopName;
+    public final String stopAddress;
+    public final String transportType;
+    public final String stopId;
+    public final double stopLat;
+    public final double stopLon;
+
+    public NearbyStopsDetails(
+            String stopName,
+            String stopAddress,
+            String transportType,
+            String stopId,
+            double stopLat,
+            double stopLon) {
+        this.stopName = stopName;
+        this.stopAddress = stopAddress;
+        this.transportType = transportType;
+        this.stopId = stopId;
+        this.stopLat = stopLat;
+        this.stopLon = stopLon;
+    }
+
+    public NearbyStopsDetails(Parcel input) {
+        stopName = input.readString();
+        stopAddress = input.readString();
+        transportType = input.readString();
+        stopId = input.readString();
+        stopLat = input.readDouble();
+        stopLon = input.readDouble();
+    }
+
+    @Override
+    public String toString() {
+        return "stopName: " + stopName +
+                "; stopAddress: " + stopAddress +
+                "; stopLat: " + stopLat +
+                "; stopLon: " + stopLon;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(stopName);
+        dest.writeString(stopAddress);
+        dest.writeString(transportType);
+        dest.writeString(stopId);
+        dest.writeDouble(stopLat);
+        dest.writeDouble(stopLon);
+    }
+
+    public static final Parcelable.Creator<NearbyStopsDetails> CREATOR =
+            new Parcelable.Creator<NearbyStopsDetails>() {
+                public NearbyStopsDetails createFromParcel(Parcel in) {
+                    return new NearbyStopsDetails(in);
+                }
+
+                public NearbyStopsDetails[] newArray(int size) {
+                    return new NearbyStopsDetails[size];
+                }
+            };
+}
