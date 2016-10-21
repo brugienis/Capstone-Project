c2d98198e4ac1406da65c32ccaf1216576cce006
Bohdan Rugienis <bohdan@rugienis.com>
2016-10-21 12:10:36 +1100

    Utility - add getLatLng(...) function.

--- app/src/main/java/au/com/kbrsolutions/melbournepublictransport/utilities/Utility.java
+++ app/src/main/java/au/com/kbrsolutions/melbournepublictransport/utilities/Utility.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import au.com.kbrsolutions.melbournepublictransport.R;
+import au.com.kbrsolutions.melbournepublictransport.data.LatLngDetails;
 
 /**
  * Created by business on 19/10/2016.
@@ -18,6 +19,22 @@ public class Utility {
                 = PreferenceManager.getDefaultSharedPreferences(context);
         return prefs.contains(context.getString(R.string.pref_key_location_latitude))
                 && prefs.contains(context.getString(R.string.pref_key_location_longitude));
+    }
+
+    public static LatLngDetails getLatLng(Context context) {
+        LatLngDetails latLngDetails = null;
+        SharedPreferences prefs
+                = PreferenceManager.getDefaultSharedPreferences(context);
+        boolean useDeviceLocationOn =
+                prefs.getBoolean(context.getString(R.string.pref_key_use_device_location), true);
+        if (!useDeviceLocationOn) {
+            latLngDetails = new LatLngDetails(
+                    prefs.getFloat(context.getString(R.string.pref_key_location_latitude), Float.NaN),
+                    prefs.getFloat(context.getString(R.string.pref_key_location_longitude), Float.NaN)
+
+            );
+        }
+        return latLngDetails;
     }
 
     public static float getLocationLatitude(Context context) {
